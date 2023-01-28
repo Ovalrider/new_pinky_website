e7713cb7e35ae74a7a954bafc95b73a0ef157cf6
Ovalrider <arten.shkoren@gmail.com>
2023-01-28 21:14:44 +0200

    created project

---
+++ animation.js
@@ -0,0 +1,18 @@
+const swipper = new Swipper(".gallerySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    breakpoints :{
+        800:{
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        600:{
+            slidesPerView: 2,
+            spaceBetween: 20, 
+        },
+    },
+});
